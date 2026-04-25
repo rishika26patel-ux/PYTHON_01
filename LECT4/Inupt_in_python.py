62d990a45baf6893ab2bb5af6c9a2183c3c3a1b9
rishika26patel <rishika26patel@gmail.com>
2026-04-26 03:43:22 +0530

    py

--- LECT4/Inupt_in_python.py
+++ LECT4/Inupt_in_python.py
@@ -1,5 +1,15 @@
-name= input("enter your name: ")
-print("welcome",name)
+#INPUT IN PYTHON
+#INPUT() STATEMENT IS USED TO ACCEPT VALUES (USING KEYBOARDS) FROM USER
+#INPUT()#RESULT FOR INPUT()IS ALWAYS A STR
 
-age= input("enter your age:")
-print("you entered",age)
+# name= input("enter your name: ")
+# print("welcome",name)
+
+# age= input("enter your age:")
+# print("you entered",age)
+
+
+#USING TYPE CASTING WE ARE CONVERT INPUT STR TYPE INTO DATA TYPE
+int("3")
+val= float(input("enter some value:"))
+print(type(val),val)
