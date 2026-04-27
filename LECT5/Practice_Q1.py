64a3df7cb7524d600789f097b1033a208eaaa50a
rishika26patel <rishika26patel@gmail.com>
2026-04-27 06:15:03 +0530

    py

---
+++ LECT5/Practice_Q1.py
@@ -0,0 +1,10 @@
+#write a program to input 2 numbers & print their sum
+# num1=2
+# num2=4
+# sum=(num1+num2)
+# print(sum)
+
+num1= int(input("enter first number:"))
+num2= int(input("enter number second:"))
+sum= (num1 + num2)
+print("your sum is",sum)
