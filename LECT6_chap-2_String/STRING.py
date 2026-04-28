c23aad5050b5d74bfeba553927ee5d1b45cce830
rishika26patel <rishika26patel@gmail.com>
2026-04-28 08:36:11 +0530

    string

---
+++ LECT6_chap-2_String/STRING.py
@@ -0,0 +1,14 @@
+#STRINGS
+#String is a data type that store a sequence of character.
+
+#BASIC OPERATIONS
+
+#  *Concatenation
+# "hello" + "world"--> "helloworld"
+
+#Length of string
+#len(str)
+
+#CREATING STRING
+str1 ="this is string \nwe are craeting it in python"
+print(str1)
