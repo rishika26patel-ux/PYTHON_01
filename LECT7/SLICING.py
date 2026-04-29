d6a8d403b53374a20ea3db6acc41406ce9182bbe
rishika26patel <rishika26patel@gmail.com>
2026-04-29 06:20:24 +0530

    code

---
+++ LECT7/SLICING.py
@@ -0,0 +1,21 @@
+# Slicing
+
+# Accesing parts of a String
+
+# str[starting_idx:ending_idx] #ending idx is not included
+
+# particular operation perform karta hai that is called functions
+
+str= "im rishika"
+print(str[5:12]) 
+# or
+print(str[5:len(str)])
+# or
+print(str[0:])
+# or
+print(str[:8])
+
+
+#NEGATIVE INDEX Counting IN SLICING
+str= "apple"
+print(str[-3:-1])
