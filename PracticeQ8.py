1ca924d9d15bd4f9d69d2ea4416c9fc0828eed97
rishika26patel <rishika26patel@gmail.com>
2026-05-01 04:40:05 +0530

    py

---
+++ PracticeQ8.py
@@ -0,0 +1,11 @@
+#write a program to find the greatest of 3 numbers entered by the user
+a= int(input("enter your first number:"))
+b= int(input("enter your second number:"))
+c= int(input("enter your third number:"))
+
+if(a>=b and a>=c):
+    print("first number is gratest",a)
+elif(b>=c):
+    print("second number is greatest",b)
+else:
+    print("third number is greatest:",c)        
