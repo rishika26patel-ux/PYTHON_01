df889ebb464c125110fdeb5afcb049fa385a6b23
rishika26patel <rishika26patel@gmail.com>
2026-05-01 03:51:10 +0530

    code

--- LECT7/PracticeQ6.py
+++ LECT7/PracticeQ6.py
@@ -1,3 +1,3 @@
-#WRITE A PROGRAM TO FIND THE OCCURRENCE OF '$' IN A STRING
-str= "rishika $$$$"
+#WRITE A PROGRAM TO FIND THE OCCURRENCE(count) OF '$' IN A STRING
+str= "hey i am the $ symbol $99.90$$$"
 print(str.count("$"))
---
+++ LECT8/Traffic_light.py
@@ -0,0 +1,8 @@
+light ="red"
+
+if(light=="red"):
+    print("stop")
+elif(light=="green"):
+    print("go")
+elif(light=="yellow"):
+    print("look")        
