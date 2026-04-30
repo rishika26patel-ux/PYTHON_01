8cbe0517b7986a23a42489320dc175c91c3b280a
rishika26patel <rishika26patel@gmail.com>
2026-05-01 04:29:51 +0530

    py

---
+++ PracticeQ7.py
@@ -0,0 +1,12 @@
+#write a program to check if a number entered by the user is odd or even.
+num= int(input("enter any number:"))
+# if("num%2==0"):
+#     print("your number is even")
+# elif("num%2=!0"):
+#     print("your number is odd")  
+rem= num%2
+if(rem==0):
+    print("EVEN")
+else:
+    print("ODD")
+         
