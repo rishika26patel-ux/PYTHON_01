2ab9830831fd9c7e42d169f33a5580860d48de23
rishika26patel <rishika26patel@gmail.com>
2026-05-02 04:43:59 +0530

    code

---
+++ LECT9_chap-3_List&Tuple/Lists_in.py
@@ -0,0 +1,9 @@
+#LISTS IN PYTHON
+#A built in data type that stores set of values
+# it can store elements of different types(integer,float,string,etc)
+marks=[99,90.6,97,86,82,75,65]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0])
+print(marks[6])
