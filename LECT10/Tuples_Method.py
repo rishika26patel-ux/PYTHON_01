b66df4d5e6b11ec4eb2a51fc65a1b7d99b6f7f48
rishika26patel <rishika26patel@gmail.com>
2026-05-02 09:07:30 +0530

    tuple

---
+++ LECT10/Tuples_Method.py
@@ -0,0 +1,10 @@
+#TUPLES METHOD
+#tup.index(el)#returns index of first occurrence [tup.index(1) is 1]
+
+#tup.count(el)#counts total occurrences [tup.count(1) is 2]
+tup=(1,4,5,7,8)
+print(tup.index(1))
+
+
+tup=(1,2,2,3,4,5)
+print(tup.count(2))
