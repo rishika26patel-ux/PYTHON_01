223066395bc4552aef424ec6bb43e68450307af1
rishika26patel <rishika26patel@gmail.com>
2026-05-03 04:38:58 +0530

    practice

---
+++ LECT10/PracticeQ12.py
@@ -0,0 +1,5 @@
+#Write a program to count the number of student with the "A" grade in the following tuple.
+#["C","D","A","A","B","B","A"]
+
+tup = ["C","D","A","A","B","B","A"]
+print(tup.count("A"))
