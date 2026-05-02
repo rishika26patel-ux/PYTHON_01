9027fd27d0d2b4e11fae6d487036396d5ba694fc
rishika26patel <rishika26patel@gmail.com>
2026-05-02 08:55:50 +0530

    tuple

---
+++ LECT10/Tuples_in.py
@@ -0,0 +1,10 @@
+#TUPLES IN PYTHON
+#A built in data type that lets us create IMMUTABLE sequences of values
+#tup=(2,3,4,5,6,8)
+#tup[0]=8 # not allowed in python
+
+tup=(2,3,4,5)
+print(type(tup))
+print(tup[0])
+print(tup[1]) #hum tupple me new value assign(kisi idx per nyi value) nii kar sakte hai kiyoki ye immutable hote hai
+print(tup[1:3])
