4e65943302575b627b90003cbc448e20865dba72
rishika26patel <rishika26patel@gmail.com>
2026-05-03 04:14:59 +0530

    tuple

---
+++ LECT10/PracticeQ10.py
@@ -0,0 +1,11 @@
+#Write a program to ask the user to enter names of their 3 favorite movies and store them in a list
+movies=[]
+movie1 = input("enter first fav movie:")
+movie2 = input("enter second fav movie:")
+movie3 = input("enter third fav movie:")
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+
+print(movies)
