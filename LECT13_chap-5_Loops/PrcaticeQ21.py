c0ef1427e03e4c7392dc480279435dc859ea6df0
rishika26patel <rishika26patel@gmail.com>
2026-05-09 04:52:16 +0530

    print list

--- LECT13_chap-5_Loops/PrcaticeQ21.py
+++ LECT13_chap-5_Loops/PrcaticeQ21.py
@@ -1,7 +1,8 @@
 #Print the elements of thefollowing list using a loop
 #[1,4,9,16,25,36,49,64,81,100]
 
-nums=[1,4,9,16,25,36,49,64,81,100]
+nums=[1,4,9,16,25,36,49,64,81,100,121]
+#traverse karna 
 idx=0
 while idx<len(nums):
     print(nums[idx])
