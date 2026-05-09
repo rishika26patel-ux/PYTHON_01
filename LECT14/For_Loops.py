b5b2cd4b1db9f780b2a6dac351e8cad0b7a7ef7b
rishika26patel <rishika26patel@gmail.com>
2026-05-09 05:53:18 +0530

    for

---
+++ LECT14/For_Loops.py
@@ -0,0 +1,45 @@
+# FOR LOOPS IN PYTHON
+#loops are used for sequencial traversal, for traversing list,string,tuples etc
+# EXAMPLE - list=[1,2,3]
+            #for el in list:
+            # print(el)
+
+
+
+#for loop print values of tuples , list
+veggies=["potato","tomato","ladyfinger","cucumber"]
+for val in veggies:
+    print(val)
+
+#print character in string
+str ="rishika patel" 
+for var in str:
+    print(var)    
+
+
+
+
+
+
+
+    #FOR LOOP WITH ELSE
+    # SYNTAX===>
+list=[1,2,3]
+for el in list:
+    print(el)
+else:
+        print("end")
+
+
+
+
+        # FIND O CHARACTER IN A STRING
+        str="rishikosika"
+
+        for char in str:
+             if(char=='o'):
+                  print("o found")
+                  break
+             print(char)
+        else:
+             print("end")
