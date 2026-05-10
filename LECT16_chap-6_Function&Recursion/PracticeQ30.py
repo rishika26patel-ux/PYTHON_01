5ada886eeb8cfbf2040662a297a17b7460ac4a93
rishika26patel <rishika26patel@gmail.com>
2026-05-10 05:31:27 +0530

    q

---
+++ LECT16_chap-6_Function&Recursion/PracticeQ30.py
@@ -0,0 +1,9 @@
+# WAF to print the length of a list(list is the parameter)
+
+cities=["delhi","gurgaon","noida","pune","mumbai","chennai"]
+heroes=["thor","ironmen","shaktiman"]
+def print_len(list):
+    print(len(list))
+
+print_len(cities)
+print_len(heroes)    
