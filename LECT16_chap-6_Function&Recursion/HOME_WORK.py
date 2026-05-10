f1c5715584ccef8eb7f40637a48e57f01715c7ba
rishika26patel <rishika26patel@gmail.com>
2026-05-10 06:18:36 +0530

    function

---
+++ LECT16_chap-6_Function&Recursion/HOME_WORK.py
@@ -0,0 +1,24 @@
+#WAF to  give a input and print string  (odd number hai to "odd") (even number hai to even)
+
+def find(n):
+    if n %2 ==0:
+        print("even")
+    else:
+        print("odd")   
+
+n= int(input("enter a number:"))
+
+find(n)        
+
+
+
+
+
+#         def calc_fact(n):
+#    fact=1
+#    for i in range(1,n+1):
+#       fact*=i
+#    print(fact)
+
+# calc_fact(5)
+   
