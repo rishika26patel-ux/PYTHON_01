bc7057295efd63cc0c169c751471890547d30247
rishika26patel <rishika26patel@gmail.com>
2026-05-26 11:43:21 +0530

    recursion

---
+++ LECT17/PracticeQ35.py
@@ -0,0 +1,12 @@
+#Write a recursive function to print all elements in a list.
+
+def print_list(list,idx=0):
+    if(idx== len(list)):
+        return
+    print(list[idx])
+    print_list(list,idx+1)
+
+
+fruits=["mnago","litchi","banana","apple"]
+
+print_list(fruits)
