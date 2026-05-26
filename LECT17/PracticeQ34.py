6b3fc09ee3c40eba9e4e267119da56b53738b92e
rishika26patel <rishika26patel@gmail.com>
2026-05-26 11:35:33 +0530

    recursion

---
+++ LECT17/PracticeQ34.py
@@ -0,0 +1,9 @@
+#Write a recursive functions to calculate the sum of first n nuatural numbers
+
+def calc_sum(n):
+    if(n==0):
+        return 0
+    return calc_sum(n-1) + n
+
+sum=calc_sum(5)    
+print(sum)
