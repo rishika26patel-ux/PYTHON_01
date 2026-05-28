5f1d4feb712095e1be993b96e554ed857216fe34
rishika26patel <rishika26patel@gmail.com>
2026-05-28 18:09:15 +0530

    oop

---
+++ LECT19_chap-8_OOPS/oop_in.py
@@ -0,0 +1,37 @@
+#OOOPS IN PYTHON
+
+# TO MAP WITH REAL WORLD SCENARIOS,we start using objects in code.
+# this is called OBJECT ORIENTED PROGRAMMING.
+
+# functions ke karan "redundancy" decrease hoti hai or "reusability" increase hoti hai
+
+
+
+
+#CLASS AND OBJECT IN PYTHON
+# ----------------------------
+# CLASS - Class is a blueprint for creating objects.
+
+# creating class
+# class Students:
+#   name="rishika patel"
+
+
+# creating object(instance)
+
+# s1=Student()
+# print(s1.name)
+
+
+#CREATING A CLASS EXAMPLE
+
+class Student:
+    name="rishika"
+
+s1=Student()
+print(s1.name)    
+
+
+
+s2=Student()
+print(s2.name) 
