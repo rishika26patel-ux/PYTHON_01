ba12c0edbc2c6c1aeaa6f8ed6ac7aa7915d1c9e3
rishika26patel <rishika26patel@gmail.com>
2026-06-04 12:01:03 +0530

    class methods

---
+++ LECT23/Class_Method.py
@@ -0,0 +1,29 @@
+# CLASS METHOD
+# a class method is bound to the class & recieves the class as an implicit first argument.
+
+# **note===>static method cant access or modify class state & generally for utility.
+
+class Person:
+    name="rishika"
+
+    def changeName(self,name):
+        self.__class__.name="tanu"
+
+p1=Person()
+p1.changeName("tanu")
+print(p1.name)
+print(Person.name)        
+
+
+# USING CLASS METHODS
+class Person:
+    name="rishika"
+     
+    @classmethod
+    def changeName(cls,name):
+        cls.name=name 
+ 
+p1=Person()
+p1.changeName("tanu")
+print(p1.name)
+print(Person.name)  
