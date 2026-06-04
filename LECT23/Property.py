fe74a41be8d681c6a6c39640c32cb00002ff4360
rishika26patel <rishika26patel@gmail.com>
2026-06-04 12:14:56 +0530

    property

---
+++ LECT23/Property.py
@@ -0,0 +1,20 @@
+# PROPERTY
+
+# We use @property decorator on any method in the class to use the method as a property
+
+class Student:
+    def __init__(self,phy,chem,math):
+        self.phy=phy
+        self.chem=chem
+        self.math=math
+
+    @property
+    def percentage(self):
+        return str((self.phy + self.chem + self.math)/3)+"%"
+
+
+stu1=Student(98,97,96)
+print(stu1.percentage)
+
+stu1.phy=86
+print(stu1.percentage)
